bcaa687d2fe381e9d89887b8d5496ce435edb4de
vleesenbotten:Q <tessasteur@gmail.com>
2023-06-15 10:32:38 +0200

    added search page and navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="/profile" element={<Profile/>}/>
+            <Route path="/search" element={<Search/>}/>
         </Routes>
       </>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from "../../components/button/Button";
-import Countdown from "../../components/Countdown";
+import Countdown from "../../components/countdown/Countdown";
 
 function Home() {
     return (
