2580f56904257d422502ad46f0b13d43bbc45bed
vleesenbotten:Q <tessasteur@gmail.com>
2023-06-23 15:00:51 +0200

    navigation basic flexbox done

--- src/App.js
+++ src/App.js
@@ -10,26 +10,28 @@ import Search from "./pages/search/Search";
 import GameDetails from "./pages/gamedetails/GameDetails";
 import Articles from "./pages/articles/Articles";
 import ArticlePost from "./pages/articlepost/ArticlePost";
-import './App.css';
-
-
+import styles from "./components/navigation/Navigation.module.css";
 
 function App() {
     const { isAuth } = useContext(AuthContext);
 
   return (
       <>
-        <Navigation/>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={isAuth ? <Profile/> : <Navigate to="/login"/>}/>
-            <Route path="/search" element={isAuth ? <Search/> : <Navigate to="/login"/>} />
-            <Route path="/articles" element={isAuth ? <Articles/> : <Navigate to="/login"/>} />
-            <Route path="/articles/:id" element={isAuth ? <ArticlePost/> : <Navigate to="/login"/>} />
-            <Route path="/game/:id" element={isAuth ? <GameDetails/> : <Navigate to="/login"/>} />
-        </Routes>
+        <div className={styles["outer-content-container"]}>
+            <Navigation/>
+        </div>
+          <div className={styles["outer-content-container"]}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={isAuth ? <Profile/> : <Navigate to="/login"/>}/>
+                <Route path="/search" element={isAuth ? <Search/> : <Navigate to="/login"/>} />
+                <Route path="/articles" element={isAuth ? <Articles/> : <Navigate to="/login"/>} />
+                <Route path="/articles/:id" element={isAuth ? <ArticlePost/> : <Navigate to="/login"/>} />
+                <Route path="/game/:id" element={isAuth ? <GameDetails/> : <Navigate to="/login"/>} />
+            </Routes>
+        </div>
       </>
   );
 }
